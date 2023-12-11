969a3df5612f64eb10f28a9c73a45f0370fe9e9e
UelintonHJ <uelinton.h@gmail.com>
2023-12-11 18:10:47 -0300

    Separando funções e métodos

--- Fokus-projeto-base/script.js
+++ Fokus-projeto-base/script.js
@@ -5,16 +5,18 @@ const longBtn = document.querySelector('.app__card-button--longo')
 const banner = document.querySelector('.app__image')
 
 focusBtn.addEventListener('click', () => {
-    html.setAttribute('data-contexto', 'foco')
-    banner.setAttribute('src', 'imagens/foco.png')
+    changeContext('foco');
 })
 
 shortBtn.addEventListener('click', () => {
-    html.setAttribute('data-contexto', 'descanso-curto')
-    banner.setAttribute('src', 'imagens/descanso-curto.png')
+    changeContext('descanso-curto')
 })
 
 longBtn.addEventListener('click', () => {
-    html.setAttribute('data-contexto', 'descanso-longo')
-    banner.setAttribute('src', 'imagens/descanso-longo.png')
+    changeContext('descanso-longo')
 })
+
+function changeContext(context) {
+    html.setAttribute('data-contexto', context);
+    banner.setAttribute('src', `/Fokus-projeto-base/imagens/${context}.png`)
+}
