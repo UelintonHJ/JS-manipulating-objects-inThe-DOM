6f18bf3a0829fc4c8d02a1a0166f91240a4a2a1b
UelintonHJ <uelinton.h@gmail.com>
2023-12-11 18:29:07 -0300

    Alterando texto da página

--- Fokus-projeto-base/script.js
+++ Fokus-projeto-base/script.js
@@ -3,6 +3,7 @@ const focusBtn = document.querySelector('.app__card-button--foco')
 const shortBtn = document.querySelector('.app__card-button--curto')
 const longBtn = document.querySelector('.app__card-button--longo')
 const banner = document.querySelector('.app__image')
+const titulo = document.querySelector('.app__title')
 
 focusBtn.addEventListener('click', () => {
     changeContext('foco');
@@ -19,4 +20,26 @@ longBtn.addEventListener('click', () => {
 function changeContext(context) {
     html.setAttribute('data-contexto', context);
     banner.setAttribute('src', `/Fokus-projeto-base/imagens/${context}.png`)
+    switch (context) {
+        case "foco":
+            titulo.innerHTML = `Otimize sua produtividade,<br>
+            <strong class="app__title-strong">mergulhe no que importa.</strong>`
+
+            break;
+
+        case "descanso-curto":
+            titulo.innerHTML = `Que tal dar uma respirada?<br>
+            <strong class="app__title-strong">Faça uma pausa curta!</strong>`
+
+            break;
+
+        case "descanso-longo":
+            titulo.innerHTML = `Hora de voltar à superfície.<br>
+            <strong class="app__title-strong">Faça uma pausa longa.</strong>`
+
+            break;
+    
+        default:
+            break;
+    }
 }
