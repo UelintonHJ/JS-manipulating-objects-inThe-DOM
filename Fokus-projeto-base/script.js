235e63be5c66ed8428f2a1fe9d00cfe5bfcd101f
UelintonHJ <uelinton.h@gmail.com>
2023-12-13 19:22:59 -0300

    Adicionando e removendo classes com ClassList

--- Fokus-projeto-base/script.js
+++ Fokus-projeto-base/script.js
@@ -1,25 +1,34 @@
-const html = document.querySelector('html')
-const focusBtn = document.querySelector('.app__card-button--foco')
-const shortBtn = document.querySelector('.app__card-button--curto')
-const longBtn = document.querySelector('.app__card-button--longo')
-const banner = document.querySelector('.app__image')
-const titulo = document.querySelector('.app__title')
+const html = document.querySelector('html');
+const focusBtn = document.querySelector('.app__card-button--foco');
+const shortBtn = document.querySelector('.app__card-button--curto');
+const longBtn = document.querySelector('.app__card-button--longo');
+const banner = document.querySelector('.app__image');
+const titulo = document.querySelector('.app__title');
+const buttons = document.querySelectorAll('.app__card-button');
 
 focusBtn.addEventListener('click', () => {
     changeContext('foco');
+    focusBtn.classList.add('active')
 })
 
 shortBtn.addEventListener('click', () => {
-    changeContext('descanso-curto')
+    changeContext('descanso-curto');
+    shortBtn.classList.add('active')
 })
 
 longBtn.addEventListener('click', () => {
-    changeContext('descanso-longo')
+    changeContext('descanso-longo');
+    longBtn.classList.add('active')
 })
 
 function changeContext(context) {
+    buttons.forEach(function(context){
+        context.classList.remove('active');
+    })
+    
     html.setAttribute('data-contexto', context);
     banner.setAttribute('src', `/Fokus-projeto-base/imagens/${context}.png`)
+    
     switch (context) {
         case "foco":
             titulo.innerHTML = `Otimize sua produtividade,<br>
