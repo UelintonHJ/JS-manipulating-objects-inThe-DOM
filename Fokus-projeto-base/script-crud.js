faee02667785b38aa70dc9fbc2d4c93705715c47
UelintonHJ <uelinton.h@gmail.com>
2023-12-16 20:06:23 -0300

    Desenvolvendo to-do list no script-crud

--- Fokus-projeto-base/script-crud.js
+++ Fokus-projeto-base/script-crud.js
@@ -1,8 +1,20 @@
 //Find the add task button
 
- const addTaskBtn = document.querySelector('.app__button--add-task')
- const addTaskForm = document.querySelector('.app__form-add-task')   
+const addTaskBtn = document.querySelector('.app__button--add-task')
+const addTaskForm = document.querySelector('.app__form-add-task')
+const textArea = document.querySelector('.app__form-textarea')
 
- addTaskBtn.addEventListener('click', () => {
+const tasks = []
+
+addTaskBtn.addEventListener('click', () => {
     addTaskForm.classList.toggle('hidden')
- })
+})
+
+addTaskForm.addEventListener('submit', (events) => {
+    events.preventDefault();
+    const task = {
+        description: textArea.value
+    }
+    tasks.push(task)
+    localStorage.setItem('tasks', tasks)
+})
