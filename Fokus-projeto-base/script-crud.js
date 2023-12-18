07cca91e9952b4070ba7db8b15d125a7a81c528b
UelintonHJ <uelinton.h@gmail.com>
2023-12-18 19:27:56 -0300

    Lendo tarefas gravadas

--- Fokus-projeto-base/script-crud.js
+++ Fokus-projeto-base/script-crud.js
@@ -1,37 +1,40 @@
-//Find the add task button
-
 const addTaskBtn = document.querySelector('.app__button--add-task')
 const addTaskForm = document.querySelector('.app__form-add-task')
 const textArea = document.querySelector('.app__form-textarea')
+const ulTasks = document.querySelector('.app__section-task-list')
 
-const tasks = []
+const tasks = JSON.parse(localStorage.getItem('tasks')) || []
 
-function createTaskElemnt(task) {
-    const createElement = document.createElement('li')
+function createTaskElement(task) {
+    const li = document.createElement('li')
     li.classList.add('app__section-task-list-item')
 
     const svg = document.createElement('svg')
     svg.innerHTML = `
-    <svg>
-        <svg class="app__section-task-icon-status" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <svg class="app__section-task-icon-status" width="24" height="24" viewBox="0 0 24 24" fill="none" 
+            xmlns="http://www.w3.org/2000/svg">
             <circle cx="12" cy="12" r="12" fill="#FFF"></circle>
-            <path d="M9 16.1719L19.5938 5.57812L21 6.98438L9 18.9844L3.42188 13.4062L4.82812 12L9 16.1719Z" fill="#01080E"></path>
+            <path d="M9 16.1719L19.5938 5.57812L21 6.98438L9 18.9844L3.42188 13.4062L4.82812 12L9 16.1719Z" 
+                fill="#01080E"></path>
         </svg>
-    </svg>
     `
 
     const paragraph = document.createElement('p')
     paragraph.textContent = task.description
+    paragraph.classList.add('app__section-task-list-item-description')
 
     const button = document.createElement('button')
+    button.classList.add('app_button-edit')
+
     const imgBtn = document.createElement('img')
-    imgBtn.setAttribute('src', '/imagens/edit.png')
+    imgBtn.setAttribute('src', '/Fokus-projeto-base/imagens/edit.png')
+    button.append(imgBtn)
 
-    button.append('imgButton')
+    li.append(svg)
+    li.append(paragraph)
+    li.append(button)
 
-    li.append('svg')
-    li.append('paragraph')
-    li.append('button')
+    return li
 }
 
 addTaskBtn.addEventListener('click', () => {
@@ -44,5 +47,14 @@ addTaskForm.addEventListener('submit', (events) => {
         description: textArea.value
     }
     tasks.push(task)
+    const taskElement = createTaskElement(task)
+    ulTasks.append(taskElement)
     localStorage.setItem('tasks', JSON.stringify(tasks))
+    textArea.value = ''
+    addTaskForm.classList.add('hidden')
 })
+
+tasks.forEach(task => {
+   const taskElement = createTaskElement(task)
+   ulTasks.append(taskElement)
+});
